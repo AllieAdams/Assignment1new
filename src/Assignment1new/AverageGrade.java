4b4f37a28d1b672beb5ada785bf5d88c5d690350
Allie <Allie@192.168.0.18>
2017-10-08 08:57:51 -0400

    Woohoo it works!!! Final product.

--- src/Assignment1new/AverageGrade.java
+++ src/Assignment1new/AverageGrade.java
@@ -4,6 +4,7 @@
  */
 package Assignment1new;
 
+import java.io.PrintStream;
 import java.util.Scanner;
 
 /**
@@ -12,12 +13,13 @@ import java.util.Scanner;
  */
 public class AverageGrade {
     public static void main (String [] args){
-        double grade1, grade2, grade3, grade4, grade5;
-        String letterGrade1 = null, letterGrade2 = null, letterGrade3 = null, letterGrade4 = null, letterGrade5 = null;
+        double grade1, grade2, grade3, grade4, grade5, avgGrade;
+        String letterGrade1, letterGrade2, letterGrade3, letterGrade4, letterGrade5;
+        String letterGradeAvg;
         
         
         
-        System.out.print("This program will calculate the average of five grades and convert them tp letters grades. \nEnter your grades in numeric form. I.e. 87 not eighty seven. \n");
+        System.out.print("This program will calculate the average of five grades and convert them to letters grades. \nEnter your grades in numeric form. I.e. 87 not eighty seven. \n");
         
         grade1= getGrade();
         grade2= getGrade();
@@ -25,14 +27,23 @@ public class AverageGrade {
         grade4= getGrade();
         grade5= getGrade();
         
-        letterGrade1= getLetterGrade(grade1, letterGrade1);
-        letterGrade2= getLetterGrade(grade2, letterGrade2);
-        letterGrade3= getLetterGrade(grade3, letterGrade3);
-        letterGrade3= getLetterGrade(grade4, letterGrade4);
-        letterGrade3= getLetterGrade(grade5, letterGrade5);
+        avgGrade= getAverage(grade1,grade2,grade3,grade4,grade5);
         
+        letterGrade1= getLetterGrade(grade1);
+        letterGrade2= getLetterGrade(grade2);
+        letterGrade3= getLetterGrade(grade3);
+        letterGrade4= getLetterGrade(grade4);
+        letterGrade5= getLetterGrade(grade5);
+        letterGradeAvg= getLetterGrade(avgGrade);
+        
+        System.out.println(letterGrade1);
+        System.out.printf("In the first course, you earned a mark of %.1f, which is a grade of %s%n", grade1, letterGrade1);
+        System.out.printf("In the second course, you earned a mark of %.1f, which is a grade of %s%n", grade2, letterGrade2);
+        System.out.printf("In the third course, you earned a mark of %.1f, which is a grade of %s%n", grade3, letterGrade3);
+        System.out.printf("In the fourth course, you earned a mark of %.1f, which is a grade of %s%n", grade4, letterGrade4);
+        System.out.printf("In the fifth course, you earned a mark of %.1f, which is a grade of %s%n", grade5, letterGrade5);
+        System.out.printf("Overall, you earned a mark of %.1f, which is a grade of %s%n", avgGrade, letterGradeAvg);
         System.out.printf("The average grade is %.1f", getAverage(grade1,grade2,grade3,grade4,grade5));
-        System.out.printf("In the first course, you earned a mark of %d, which is a grade of %s%n", grade1, letterGrade1);
     }
         //This method will prompt user for grade and return the value as a double
      public static double getGrade(){
@@ -43,24 +54,23 @@ public class AverageGrade {
      public static double getAverage(double g1, double g2, double g3, double g4, double g5){
          return (g1+g2+g3+g4+g5)/5;
      }
-     public static String getLetterGrade(double g, String letterGrade1){
+     public static String getLetterGrade(double g){
        
          if (g >= 80.0){
-           letterGrade1 = "A";
+           return "A";
          }
          else if (g >= 70.0){
-           letterGrade1 = "B";
+           return "B";
          }
          else if (g >= 60.0){
-           letterGrade1 = "C";
+           return "C";
          }
          else if (g >= 50.0){
-           letterGrade1 = "D";
+           return "D";
          }
          else {
-           letterGrade1 = "F";
+           return "F";
          }
         
-         return letterGrade1;
      }
 }
